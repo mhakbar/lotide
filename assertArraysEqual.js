dfcd033556b4a3bf02b293a41d7231e0a85ce17b
mhakbar <habibakbar@live.com>
2021-11-24 13:23:16 -0800

    completed eqArrays and assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,3 +13,9 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  else return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // => Assertion Passed
+assertArraysEqual(['1', '2', '3',], [1, 2]) // => Assertion Failed
