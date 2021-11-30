2202adc9ea83f4a9ca3c0dd03519f170c39461b5
mhakbar <habibakbar@live.com>
2021-11-30 05:21:58 -0800

    assignment for week 2, day 2 tail.js completed

--- tail.js
+++ tail.js
@@ -1,15 +1,16 @@
-const assertEqual = function(actual, expected, msg, msg2) {
+// const assertEqual = function(actual, expected, msg, msg2) {
 
-  msg = "Assertion failed: ";
-  msg2 = "Assertion Passed: ";
+//   msg = "Assertion failed: ";
+//   msg2 = "Assertion Passed: ";
 
-  if (actual === expected) {
-    console.log(msg2 + actual + " === " + expected);
-  } else {
-    console.log(msg + actual + " !== " + expected);
-  }
+//   if (actual === expected) {
+//     console.log(msg2 + actual + " === " + expected);
+//   } else {
+//     console.log(msg + actual + " !== " + expected);
+//   }
 
-};
+// }; //Commenting out the assertEqual code as a requirement for week 2 day 2. We learnt how to export code this week.
+const assertEqual = require('./assertEqual'); //(function will be called from the assertEqual file)
 
 const tail = function(size, size2) {
   size2 = size.length - 1;
@@ -17,9 +18,12 @@ const tail = function(size, size2) {
 
 
 
-};
+}
+module.exports = assertEqual;
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Labs");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Labs");
