f61cc293770c7e86d54fc841d77721f716071a0f
mhakbar <habibakbar@live.com>
2021-11-30 07:13:09 -0800

    adding refractored code

--- middle.js
+++ middle.js
@@ -1,22 +1,26 @@
-const eqArrays = (arr1, arr2) => {
-  // check if the array lengths are different
-  if (arr1.length !== arr2.length) return false;
-  // loop through elements in an array to each other
-  for (let i = 0, j = 0; i < arr1.length; j++,i++) {
-    // check whether the two elements of the array are equal
-    if (arr1[i] === arr2[j]) {
-      continue;
-    } else {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  else return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-};
+
+// const eqArrays = (arr1, arr2) => {
+//   // check if the array lengths are different
+//   if (arr1.length !== arr2.length) return false;
+//   // loop through elements in an array to each other
+//   for (let i = 0, j = 0; i < arr1.length; j++,i++) {
+//     // check whether the two elements of the array are equal
+//     if (arr1[i] === arr2[j]) {
+//       continue;
+//     } else {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// const assertArraysEqual = (arr1, arr2) => {
+//   if (eqArrays(arr1, arr2)) return console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+//   else return console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+// };
 
 const middle = function(array1) {
 
@@ -43,7 +47,9 @@ const middle = function(array1) {
 
 };
 
-console.log(assertArraysEqual(middle([1, 2, 3]),[2]));
-console.log(assertArraysEqual(middle([1, 2]),[]));
-console.log(assertArraysEqual(middle([1]),[]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]));
+module.exports = middle;
+
+// console.log(assertArraysEqual(middle([1, 2, 3]),[2]));
+// console.log(assertArraysEqual(middle([1, 2]),[]));
+// console.log(assertArraysEqual(middle([1]),[]));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]));
