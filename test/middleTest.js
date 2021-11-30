b1265ac31db55cc38190bfeca4c88ab15d91283b
mhakbar <habibakbar@live.com>
2021-11-30 08:29:16 -0800

    git tail and git middle refactor using chai mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,23 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual')
+// const middle = require('../middle');
+// const assertArraysEqual = require('../assertArraysEqual')
 
-console.log(assertArraysEqual(middle([1, 2, 3]),[2]));
-console.log(assertArraysEqual(middle([1, 2]),[]));
-console.log(assertArraysEqual(middle([1]),[]));
-console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]));
+// console.log(assertArraysEqual(middle([1, 2, 3]),[2]));
+// console.log(assertArraysEqual(middle([1, 2]),[]));
+// console.log(assertArraysEqual(middle([1]),[]));
+// console.log(assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]));
+
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns Lighthouse for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.strictEqual(middle(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse']);
+    
+  });
+
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+     assert.strictEqual(middle([1, 2, 3, 4]),[2, 3]); 
+   });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,25 @@
-const assertEqual = require('..//assertEqual');
-const tail = require('..//tail');
+// const assertEqual = require('..//assertEqual');
+// const tail = require('..//tail');
 
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Labs");
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), "Labs");
+
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns Labs for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.strictEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), 'Labs');
+    
+  });
+
+  it("returns '6' for ['5', '6']", () => {
+     assert.strictEqual(tail(['5', '6']), '6'); 
+   });
+
+});
